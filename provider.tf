e93e74d9f138b0a1eb24444103baf1944d67ad7f
K.Vinay Reddy <vinayreddykovuri@gmail.com>
2025-11-20 12:32:30 +0530

    Vinay

--- provider.tf
+++ provider.tf
@@ -2,8 +2,8 @@ terraform {
   required_version = "1.13.5"
   required_providers {
     aws = {
-        source = "harshicorp/aws"
-        version = "1.42.76"
+         source = "hashicorp/aws"
+         version = "6.21.0"
     }
   }
 }
